e637994bd01cda81fad709594d7fc63d005cb192
kimbusung <superbsung@gmail.com>
2016-04-12 00:41:49 +0900

    2nd commit

--- hello.go
+++ hello.go
@@ -4,4 +4,14 @@ import "fmt"
 
 func main() {
     fmt.Println("Hello World")
+    
+    for i := 1 ; i < 10 ; i++ {
+        fmt.Println(i)
+    } 
+    
+    for i := 1 ; i < 9 ; i++ {
+        for j := 1 ; j < 9 ; j++ {
+            print(i*j)
+        }
+    }
 }
